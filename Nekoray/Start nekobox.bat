8e1a8e08de5d89b362303f4810c2a3e9719c63ea
leonid.zhukov <leonid.zhukov@guestmg.pro>
2026-03-28 20:09:31 +0300

    обновление

--- Nekoray/Start nekobox.bat
+++ Nekoray/Start nekobox.bat
@@ -4,18 +4,14 @@ echo Обновление...
 cd /d "%~dp0"
 
 :: Сохраняем профили
-xcopy /E /I /Y "%~dp0config\profiles" "%~dp0profiles_backup"
-
-:: Удаляем проблемные файлы
-del /F /Q "%~dp0config\groups\nekobox.json"
-del /F /Q "%~dp0config\neko.log"
+xcopy /E /I /Y "%~dp0Nekoray\config\profiles" "%~dp0profiles_backup"
 
 :: Обновляем
 git pull
 
 :: Восстанавливаем профили
-xcopy /E /I /Y "%~dp0profiles_backup" "%~dp0config\profiles"
+xcopy /E /I /Y "%~dp0profiles_backup" "%~dp0Nekoray\config\profiles"
 rmdir /S /Q "%~dp0profiles_backup"
 
 echo Запуск NekoBox...
-start "" "%~dp0nekobox.exe"
+start "" "%~dp0Nekoray\nekobox.exe"

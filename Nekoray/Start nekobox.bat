a1589d68bd5093193c5a49c2e249df13e5245866
leonid.zhukov <leonid.zhukov@guestmg.pro>
2026-03-28 15:12:28 +0300

    обновление

--- Nekoray/Start nekobox.bat
+++ Nekoray/Start nekobox.bat
@@ -1,7 +1,17 @@
 @echo off
 chcp 65001 > nul
-echo Обновление...Перед запуском.. 
+echo Обновление...перед запуском.
 cd /d "%~dp0"
+
+:: Сохраняем профили
+xcopy /E /I /Y "%~dp0config\profiles" "%~dp0profiles_backup"
+
+:: Обновляем
 git pull
+
+:: Восстанавливаем профили
+xcopy /E /I /Y "%~dp0profiles_backup" "%~dp0config\profiles"
+rmdir /S /Q "%~dp0profiles_backup"
+
 echo Запуск NekoBox...
 start "" "%~dp0nekobox.exe"
